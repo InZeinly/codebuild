fedeff1412ab6563ecde8a7ca994c5a825d945ca
InZeinly <tuzar11@hotmail.com>
2022-11-14 13:27:36 +0200

    backend

--- backend.tf
+++ backend.tf
@@ -1,16 +1,16 @@
-# terraform {
-#     required_providers {
-#         aws = {
-#             source = "hashicorp/aws"
-#             version = "~> 3.35"
-#         }
-#     }
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.35"
+        }
+    }
 
 
-#     backend "s3" {
-#         encrypt = true
-#         bucket  = "s3bucketasdasdasdasd123"
-#         region  = "eu-central-1"
-#         key     = "state"
-#     }
-# }
+    backend "s3" {
+        encrypt = true
+        bucket  = "s3bucketasdasdasdasd123"
+        region  = "eu-central-1"
+        key     = "state"
+    }
+}
