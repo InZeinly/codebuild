fea49a5590c14ed8d0e598f0694dc0253b3ed306
InZeinly <tuzar11@hotmail.com>
2022-11-14 19:39:27 +0200

    refract app.py

--- app/app.py
+++ app/app.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 
 # list of cat images
 images = [
-  "https://media1.giphy.com/media/BzyTuYCmvSORqs1ABM/giphy.gif?cid=ecf05e47a61pohrnxvkjt8ax30omfxqbla893upt6n822900&rid=giphy.gif&ct=g"
+    "https://media1.giphy.com/media/BzyTuYCmvSORqs1ABM/giphy.gif?cid=ecf05e47a61pohrnxvkjt8ax30omfxqbla893upt6n822900&rid=giphy.gif&ct=g"
 ]
+
+
 @app.route('/')
 def index():
-  url = random.choice(images)
-  return render_template('index.html', url=url)
+    url = random.choice(images)
+    return render_template('index.html', url=url)
+
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
